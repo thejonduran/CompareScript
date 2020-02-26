8c509c0a595a48ac5dd0d89d254d7a3276b55b13
Jonathan Duran <jonathaneugeneduran@gmail.com>
2020-02-26 16:20:37 +0000

    Promisify Functions

--- index.js
+++ index.js
@@ -5,33 +5,49 @@ var params = {
 	mainFileDelimiter: "\n",
 	secondaryFile: "./data1.txt",
 	secondaryFileDelimiter: "\n",
-	includes: true
+	includes: false
 }
 
 //---------------------------------------------------------//
 
 function getData(file) {
-	var data = fs.readFileSync(file, "utf8")
-	return data.split(params.mainFileDelimiter)
+	return new Promise(function(resolve) {
+		var data = fs.readFileSync(file, "utf8")
+		var splitdata = data.split(params.mainFileDelimiter)
+		resolve(splitdata)
+	})
 }
 
 function compareData(file1, file2) {
-	var found = []
-	for (var i = 0; i < file1.length; i++) {
-		console.log("Checking: " + file1[i])
-		if (file2.includes(file1[i]) === params.includes) {
-			found.push(file1[i])
+	return new Promise(function(resolve) {
+		var found = []
+		for (var i = 0; i < file1.length; i++) {
+			console.log("Checking: " + file1[i])
+			if (file2.includes(file1[i]) === params.includes) {
+				found.push(file1[i])
+			}
 		}
-	}
-	return found;
+		resolve(found)
+	})
 }
 
 function writeToFile(array) {
-	var file;
-	for (var i = 0; i < array.length; i++) {
-		file += array[i] + "\n"
-	}
-	fs.writeFileSync("./results.txt", file, "utf8")
+	return new Promise(function(resolve) {
+		var file;
+		for (var i = 0; i < array.length; i++) {
+			file += array[i] + "\n"
+		}
+		fs.writeFileSync("./results.txt", file, "utf8")
+		resolve(console.log(file))
+	})
 }
 
-writeToFile(compareData(getData(params.mainFile), getData(params.secondaryFile)))
+async function runScript(){
+	var data1 = await getData(params.mainFile)
+	var data2 = await getData(params.secondaryFile)
+	var results = await compareData(data1,data2)
+	await writeToFile(results)
+	console.log(results)
+}
+
+runScript()
