1d98a48d398f8541a1bffb4565de21f5e7758230
Jonathan Duran <jonathaneugeneduran@gmail.com>
2020-02-26 16:05:25 +0000

    Refactor

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 var fs = require("fs")
 
 var params = {
-	mainFile:"./data2.txt",
-	mainFileDelimiter:"\n",
-	secondaryFile:"./data1.txt",
-	secondaryFileDelimiter:"\n",
-	includes:false
+	mainFile: "./data2.txt",
+	mainFileDelimiter: "\n",
+	secondaryFile: "./data1.txt",
+	secondaryFileDelimiter: "\n",
+	includes: true
 }
 
 //---------------------------------------------------------//
 
-var data1 = (function(){
-  var data = fs.readFileSync(params.mainFile,"utf8")
-  return data.split(params.mainFileDelimiter)
-})()
+function getData(file) {
+	var data = fs.readFileSync(file, "utf8")
+	return data.split(params.mainFileDelimiter)
+}
 
-var data2 = (function(){
-  var data = fs.readFileSync(params.secondaryFile,"utf8")
-  var splitdata = data.split(params.secondaryFileDelimiter)
-  //var formatdata = []
-  //for (var i = 0; i < splitdata.length; i++){
-  //  formatdata.push(splitdata[i].slice(8,-3))
-  //}
-  return splitdata
-})()
-
-var found = []
-
-for(var i = 0; i < data1.length; i++){
-  console.log("Checking: " + data1[i])
-  if(data2.includes(data1[i]) === params.includes){
-		found.push(data1[i])
+function compareData(file1, file2) {
+	var found = []
+	for (var i = 0; i < file1.length; i++) {
+		console.log("Checking: " + file1[i])
+		if (file2.includes(file1[i]) === params.includes) {
+			found.push(file1[i])
+		}
 	}
+	return found;
 }
 
-var foundFile;
-
-for (var j = 0; j < found.length; j++){
-	foundFile += found[j] + "\n"
+function writeToFile(array) {
+	var file;
+	for (var i = 0; i < array.length; i++) {
+		file += array[i] + "\n"
+	}
+	fs.writeFileSync("./results.txt", file, "utf8")
 }
 
-fs.writeFileSync("./results.txt",foundFile,"utf8")
-
-console.log(foundFile)
+writeToFile(compareData(getData(params.mainFile), getData(params.secondaryFile)))
