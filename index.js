9322e14261731d9c73a66759b4b0b8c1c1d5460a
Jonathan Duran <jonathaneugeneduran@gmail.com>
2020-02-26 17:49:34 +0000

    Convert to Async/Await

--- index.js
+++ index.js
@@ -8,41 +8,7 @@ var params = {
 	includes: false
 }
 
-//---------------------------------------------------------//
-
-function getData(file) {
-	return new Promise(function(resolve) {
-		var data = fs.readFileSync(file, "utf8")
-		var splitdata = data.split(params.mainFileDelimiter)
-		resolve(splitdata)
-	})
-}
-
-function compareData(file1, file2) {
-	return new Promise(function(resolve) {
-		var found = []
-		for (var i = 0; i < file1.length; i++) {
-			//console.log("Checking: " + file1[i])
-			if (file2.includes(file1[i]) === params.includes) {
-				found.push(file1[i])
-			}
-		}
-		resolve(found)
-	})
-}
-
-function writeToFile(array) {
-	return new Promise(function(resolve) {
-		var file;
-		for (var i = 0; i < array.length; i++) {
-			file += array[i] + "\n"
-		}
-		fs.writeFileSync("./results.txt", file, "utf8")
-		resolve()
-	})
-}
-
-async function runScript(){
+async function runScript(params){
 	var data1 = await getData(params.mainFile)
 	console.log("Got Data1...")
 	var data2 = await getData(params.secondaryFile)
@@ -55,4 +21,27 @@ async function runScript(){
 	console.log("Done.")
 }
 
-runScript()
+async function getData(file) {
+		var data = fs.readFileSync(file, "utf8")
+		var splitdata = data.split(params.mainFileDelimiter)
+		return splitdata
+}
+
+async function compareData(file1, file2) {
+		var found = []
+		for (var i = 0; i < file1.length; i++) {
+			//console.log("Checking: " + file1[i])
+			if (file2.includes(file1[i]) === params.includes) {
+				found.push(file1[i])
+			}
+		}
+		return found
+}
+
+async function writeToFile(array) {
+		var file;
+		for (var i = 0; i < array.length; i++) {
+			file += array[i] + "\n"
+		}
+		fs.writeFileSync("./results.txt", file, "utf8")
+}
